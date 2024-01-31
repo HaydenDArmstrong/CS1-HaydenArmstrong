7b09c68bb688ad3c291e4b90fc5ba80fd89ee514
Hayden Armstrong <hdarmstrong@mavs.coloradomesa.edu>
2024-01-31 15:49:17 -0700

    update

--- Assignments/Notes- Data Types and Variables/Variables.cpp
+++ Assignments/Notes- Data Types and Variables/Variables.cpp
@@ -4,11 +4,32 @@ using namespace std;
 
 int main(void)
 {
+    char middleinitial = 'D';
     int exampleInteger;
-    double exampleDouble
-    float exampleFlout
-    cout << "Hello" << endl;
+    double exampleDouble;
+    float exampleFloat;
+    const double pi = 3.14159; // constant is unmodifiable, if i do pi = 4; , will receive an error
+    string name = "Hayden";
+    /* 
+    basic escape characters 
+    \n - new line
+    \ - back slash
+    \t - tab
+    \r - carriage return
+    \' - single quote
+    \" - double quote
+    */
+    string phrase = "\n\n\n new line \n\n\n";
+    string phrasereturn = "\r carriage return /r";
+
+    exampleDouble = 50; // assigned variable to 50
+
+    cout << "Hello " << name << " " << middleinitial << endl;
     cout << "Integer Example: " << exampleInteger << endl;
     cout << "Double Example " << exampleDouble << endl;
+
+    cout << phrase << endl;
+    cout << phrasereturn << endl;
+
     return 0;
 }
