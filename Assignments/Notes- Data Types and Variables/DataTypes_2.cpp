a77ee0be588b0b7647dfaf8433264f0b1482451a
Hayden Armstrong <hdarmstrong@mavs.coloradomesa.edu>
2024-01-31 15:15:42 -0700

    Clarify

---
+++ Assignments/Notes- Data Types and Variables/DataTypes_2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int number = 0xFFFFFFFF+1;
+    int number2;
+    cout << "Hello" << endl;
+    cout << "number = " << number << endl;
+    cout << "number 2 has a random value, = " << number2 << endl;
+    return 0;
+}
---
+++ Assignments/Notes- Data Types and Variables/Variables.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int exampleInteger;
+    double exampleDouble
+    float exampleFlout
+    cout << "Hello" << endl;
+    cout << "Integer Example: " << exampleInteger << endl;
+    cout << "Double Example " << exampleDouble << endl;
+    return 0;
+}
