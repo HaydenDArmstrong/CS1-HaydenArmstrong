3c8f23a3edcde7412dfa17f46a6b8f9b5678f4db
Hayden Armstrong <hdarmstrong@mavs.coloradomesa.edu>
2024-02-01 15:46:14 -0700

    added ASCII Lab

--- Labs/ascii1/main.cpp
+++ Labs/ascii1/main.cpp
@@ -21,24 +21,38 @@ using namespace std; //resolve cout, cin, and endl names
 int main()
 {
     //FIXME3: declare a variable to store name
+    string name;
     //FIXME4: prompt user to enter their name and store the value into name variable using getline function
-    
-    //FIXME5: greet the name using the variable as the following output
-    //must output: Nice meeting you, <name>!
+    cout << "Please enter your name: ";
+    cin >> name;
+        //FIXME5: greet the name using the variable as the following output
+        //must output: Nice meeting you, <name>!
+        cout << "Nice meeting you, " << name << "!" << endl;
+        cout << "Hope you like my ASCII art...\n\n";
 
-    cout << "Hope you like my Tom & Jerry ASCII art...\n\n";
+        string line1 = "    |\\_/|        **********************    (\\\\___//)  \n";
+        string line2 = "    (--_--)      **********************    {-o---o-}    \n";
+        string line3 = "   _|    |__     **********************    {  -0-  }    \n";
+        string line4 = "__/ |  . |  \\__  **********************    {       }    \n";
+        string line5 = "    |  _ |       **********************    {       }    \n";
+        string line6 = "    | || |       **********************    {   _   }~~  \n";
+        string line7 = "    |__|__|      **********************     |_| |_|     \n";
 
-    string line1 = "  |\\_/|   **********************    (\\_/)\n";
-    cout << line1;
+        cout << line1;
+        cout << line2;
+        cout << line3;
+        cout << line4;
+        cout << line5;
+        cout << line6;
+        cout << line7;
 
-    //FIXME6: use variable to print the second line line of the graphic
-    //FIXME7: print the third line of the graphics
-    //FIXME8: use variable to print the fourth line
-    //FIXME9: use variable to print the fifth line
-    //Note: You can add more lines or print more ASCII arts of your choice if you'd like...
 
-    cout << "\nGood bye... hit enter to exit the program: " << '\n';
-    //FIXME10: make the console wait for user input
+        //FIXME6: use variable to print the second line line of the graphic
+        //FIXME7: print the third line of the graphics
+        //FIXME8: use variable to print the fourth line
+        //FIXME9: use variable to print the fifth line
+        //Note: You can add more lines or print more ASCII arts of your choice if you'd like...
 
-    return 0; //exit program by returning 0 status to the system
-}
+        cout << "\nGood bye... hit enter to exit the program: " << '\n';
+         //FIXME10: make the console wait for user input
+    }
