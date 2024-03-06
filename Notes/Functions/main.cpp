bf3d057029c8714d184961ec4e2ae24dfc73494a
Hayden Armstrong <hdarmstrong@mavs.coloradomesa.edu>
2024-03-06 15:02:01 -0700

    as

---
+++ Notes/Functions/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void sayHello(string name)// returns nothign, takea  string called name in this function as a paremeter
+{
+}
+int main(void)
+{
+    int i;
+    for (i = 1; i < 4; i++)
+    {
+    }
+
+
+    string name = "Hayden";
+
+    return 0;
+}
